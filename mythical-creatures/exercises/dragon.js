22d85f6021312a98259dfeb7a0cb9fe809a06861
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-01-24 13:23:13 -0700

    dragon tests passing

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
+class Dragon {
 
+  constructor(name, rider = null) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.timesEaten = 0;
+  }
+
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    this.timesEaten ++;
+    if (this.timesEaten > 2){
+      this.hungry = false;
+    }
+  }
+}
+
+module.exports = Dragon;
