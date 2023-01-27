a61abdeeebcace5aa6aaeebee673d780caac514a
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-01-27 11:28:57 -0700

    craft tests passing, first time using .forEach

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,26 @@
+var Material = require('./material')
+
 class Craft {
 
+  constructor({type, materials = []}) {
+    this.name = type;
+    this.materials = materials;
+    this.completed = false;
+  }
+
+  completeCraft() {
+    this.completed = true;
+    return `All done! It looks great!`;
+  }
+
+  calculateProjectTotal() {
+    var totalCost = 0;
+
+    this.materials.forEach((material) => {
+      totalCost += material.calculateMaterialCost();
+    });
+    return totalCost;
+  }
 }
 
 module.exports = Craft;
