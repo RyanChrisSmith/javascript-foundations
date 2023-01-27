f08d1cf212304fabfa0b33a0cb5d914f5d30c240
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-01-27 13:38:33 -0700

    roadrace tests passing, use of helper methods

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,26 @@
 class Roadrace {
 
+  constructor({title, city}) {
+    this.name = title;
+    this.location = city;
+    this.distance = null;
+    this.participants = [];
+  }
+
+  setDistance(miles) {
+    this.distance = miles;
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+  }
+
+  registerParticipants(runner) {
+    this.participants.push(runner);
+  }
+
+  completeRace() {
+    this.participants.forEach((participant) => {
+      participant.runRace(this.name, this.distance);
+    })
+  }
 }
 
 module.exports = Roadrace;
