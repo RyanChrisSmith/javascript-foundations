5e8a18da32b65dcd724991b9322a8f88460ac04c
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-02-28 15:45:22 -0700

    ski-lift tests passing

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,38 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
+
+  constructor(limit) {
+    this.limit = limit;
+    this.inService = true;
+    this.skiers = [];
+    this.safetyBar = 'up';
+  }
+
+  admitSkier(name, hasLiftTicket) {
+    if (!hasLiftTicket) {
+      return `Sorry, ${name}. You need a lift ticket!`
+    }
+    if (this.skiers.length < this.limit) {
+      let skier = new Skier(name, hasLiftTicket);
+      this.skiers.push(skier);
+    }
+    return `Sorry, ${name}. Please wait for the next lift!`
+  }
+
+  startLift() {
+    if (this.limit > this.skiers.length) {
+      let difference = this.limit - this.skiers.length;
+      if (difference > 1) {
+        return `We still need ${difference} more skiers!`;
+      } else {
+        return `We still need 1 more skier!`
+      }
+    }
+    if (this.limit === this.skiers.length) {
+      this.safetyBar = 'down';
+    }
+  }
 }
 
 module.exports = Lift;
