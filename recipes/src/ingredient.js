accbe98a47f321b9b1484c1e87bce1214653dd06
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-02-28 12:07:25 -0700

    ingredient and recipe tests passing, need to finish chef tests

--- recipes/src/ingredient.js
+++ recipes/src/ingredient.js
@@ -1,5 +1,13 @@
 class Ingredient {
   
+  constructor({name, amount}) {
+    this.name = name;
+    this.amount = amount;
+  }
+
+  changeAmount(newAmount) {
+    this.amount = newAmount;
+  }
 }
 
 module.exports = Ingredient;
--- recipes/src/recipe.js
+++ recipes/src/recipe.js
@@ -1,5 +1,29 @@
 class Recipe {
 
+  constructor({name, ingredients}) {
+    this.name = name;
+    this.ingredients = ingredients;
+    this.attempted = false;
+    this.rating = null;
+  }
+
+  rateRecipe(newRating) {
+    this.attempted = true;
+    this.rating = newRating;
+  }
+
+  changeIngredientAmount(name, amount) {
+    const ingredient = this.ingredients.find((ingredient) => ingredient.name === name);
+    ingredient.amount = amount;
+  }
+
+  generateGroceryList() {
+    let groceryList = 'You need:';
+    this.ingredients.forEach(ingredient => {
+      groceryList += ` ${ingredient.amount} ${ingredient.name},`;
+    });
+    return groceryList;
+  }
 }
 
 module.exports = Recipe;
