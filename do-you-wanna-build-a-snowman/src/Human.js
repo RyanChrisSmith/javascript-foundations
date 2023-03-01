ab8cf834c0467304ae7c720c101a7e74b1bc8f3e
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-03-01 14:00:40 -0700

    human tests passing

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -2,6 +2,31 @@ var Snowman = require('./Snowman');
 
 class Human {
 
+  constructor(name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0,
+    };
+  }
+
+  gatherMaterials(material, quantity) {
+    this.materials[material] += quantity;
+  }
+
+  buildASnowman() {
+    return new Snowman(this.materials)
+  }
+
+  placeMagicHat(snowman) {
+    if (snowman.canWearMagicHat()) {
+      return `Woah, this snowman is coming to life!`;
+    }
+    return `I guess I didn\'t build it correctly.`
+  }
 }
 
 module.exports = Human;
