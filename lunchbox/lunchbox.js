dcd0ff7cf914679d05a1ede76503c7ae54e4e28b
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-03-06 14:56:25 -0700

    lunchbox tests passing

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,30 @@
 class LunchBox {
 
+  constructor({owner, madeOf, shape, color}) {
+    this.owner = owner;
+    this.material = madeOf;
+    this.shape = shape;
+    this.color = color;
+    this.snacks = [];
+  }
+
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+
+  findHealthySnacks() {
+    let healthySnacks = [];
+
+    for (let i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy()) {
+        healthySnacks.push(this.snacks[i].type);
+      }
+    }
+    return healthySnacks;
+
+    // return this.snacks.filter(snack => snack.checkForHealthy()).map(snack => snack.type);
+  }
 }
 
 module.exports = LunchBox;
