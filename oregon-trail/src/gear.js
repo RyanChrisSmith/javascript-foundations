a5554abd741a116730127b7e8902594b194c1f97
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-03-07 14:05:50 -0700

    gear tests passed in oregon trail

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,5 +1,20 @@
 class Gear {
 
+  constructor(type, quantity) {
+    this.type = type;
+    this.quantity = quantity;
+  }
+
+  checkForValidity() {
+    let validTypes = ['food', 'ammunition', 'clothes'];
+
+    if (validTypes.includes(this.type)) {
+      return `Great, we\'ll need lots of ${this.type}!`;
+    }
+    let variable = `I don\'t think a ${this.type} will help us.`;
+    this.type = null;
+    return variable;
+  }
 }
 
 module.exports = Gear;
